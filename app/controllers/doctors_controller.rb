b588c75d883b47e06795f8325e3d7a024f4a9336
Nabil Hayet <nabil.bracu@gmail.com>
2020-07-01 01:29:33 +0000

    writting controller for doctor model

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,10 +1,49 @@
 class DoctorsController < ApplicationController
 
-  configure do
-    enable :sessions unless test?
-    set :session_secret, "secret"
+  get '/registrations/doctor' do
+    erb :'doctors/registration/doctor'
   end
 
-  
+  post '/registrationss' do
+    @doctor = Doctor.find_by(email: params["email"])
+    if @doctor
+      flash.next[:message] = "Email address already exists."
+      redirect 'doctors/registration/doctor'
+    else
+      @doctor = Doctor.new(name: params["name"], email: params["email"], password: params["password"])
+      @doctor.save
+      flash.next[:message] = "Successfully registered."
+      session[:doctor_id] = @doctor.id
+      redirect '/home/doctor'
+    end
+  end
+
+  get '/login/doctor' do
+    erb :'/doctors/login/doctor'
+  end
+
+  post '/logins' do
+    @doctor = Doctor.find_by(email: params[:email])
+    if @doctor
+      session[:doctor_id] = @doctor.id
+      redirect '/profile/doctor'
+    end
+    redirect '/login/doctor'
+  end
+
+  get '/profile/doctor' do
+    erb :'/doctors/show'
+  end
+
+  get '/sessionss/logout' do
+    session.clear
+    erb :'/doctors/home/doctor'
+  end
+
+  get '/home/doctor' do
+    erb :'/doctors/home/doctor'
+  end
+
+
 
 end
