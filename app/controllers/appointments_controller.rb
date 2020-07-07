841ee9cb097a6c629d278c912f3490b22e8bb97f
Nabil Hayet <nabil.bracu@gmail.com>
2020-07-07 23:04:24 +0000

    refactor design

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -165,6 +165,7 @@ class AppointmentsController < ApplicationController
 
   get '/view/doctor' do
     if ApplicationController.is_logged_in?(session)
+
       @doctor = ApplicationController.current_user(session)
       @apt = @doctor.appointments
         if !@apt.empty?
